f7ca9a13159d386e47cb36900f49d0e555312468
Nuhamin Alemayehu <nuhaminae@gmail.com>
2025-07-19 18:24:13 +0300

    init: black, isort, and flask8 formatting script

--- format.ps1
+++ format.ps1
@@ -1,11 +1,13 @@
 # Windows PowerShell version (format.ps1)
 # Run .\format.ps1 in bash
-.\.fraudvenv\Scripts\black . 
-.\.fraudvenv\Scripts\isort . 
+.\.fraudvenv\Scripts\black . --exclude .fraudvenv
+.\.fraudvenv\Scripts\isort . --skip .fraudvenv
 .\.fraudvenv\Scripts\python.exe -m flake8 . --exclude=.fraudvenv
 if (Get-ChildItem -Recurse -Filter *.ipynb) {
+  Write-Host "Formatting notebooks with black and isort via nbqa..."
   .\.fraudvenv\Scripts\python.exe -m nbqa black . --line-length=88 --exclude .fraudvenv
   .\.fraudvenv\Scripts\python.exe -m nbqa isort . --skip .fraudvenv
+  Write-Host "Linting notebooks with flake8 via nbqa..."
   .\.fraudvenv\Scripts\python.exe -m nbqa flake8 . --exclude .fraudvenv --exit-zero
 } else {
   Write-Host "No notebooks found - skipping nbQA."
